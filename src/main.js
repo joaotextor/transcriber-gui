76ae20359ec24c011323cf40d1528d3ba56edb92
João Textor <joaotextor@gmail.com>
2024-12-07 08:51:55 -0300

    install ini

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 600,
     height: 300,
+    resizable: false,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
